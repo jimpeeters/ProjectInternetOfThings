7e4211089627dcb43a5bd987963339511df688d4
Jim Peeters <jempipeeters@hotmail.com>
2016-01-23 11:23:15 +0000

    use .env

--- website/config/database.php
+++ website/config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => '127.0.0.1',
-            'database'  => 'c9',
-            'username'  => 'root',
-            'password'  => '',
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'database'  => env('DB_DATABASE', 'c9'),
+            'username'  => env('DB_USERNAME','root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
